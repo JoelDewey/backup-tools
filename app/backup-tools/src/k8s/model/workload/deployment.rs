6fe67baccbf9c8a09c61993218fa6b436fbe8f74
Joel Dewey <hello@joeldewey.com>
2023-10-14 21:52:58 -0400

    * Fix typo in default value in Helm chart. * Tweak Serde container naming.

--- app/backup-tools/src/k8s/model/workload/deployment.rs
+++ app/backup-tools/src/k8s/model/workload/deployment.rs
@@ -2,6 +2,7 @@ use crate::k8s::model::object_meta::ObjectMeta;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Deployment {
     pub api_version: String,
     pub kind: String,
@@ -11,11 +12,13 @@ pub struct Deployment {
 }
 
 #[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct DeploymentSpec {
     pub replicas: Option<i32>,
 }
 
 #[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct DeploymentStatus {
     pub replicas: i32,
     pub available_replicas: i32,

664a3cc597dffa08fc2bad6276a7758a12f182c2
Joel Dewey <hello@joeldewey.com>
2024-02-03 20:27:18 -0500

    Address breaking changes in `rustls-pemfile`.

--- app/backup-tools/src/k8s/cert.rs
+++ app/backup-tools/src/k8s/cert.rs
@@ -1,7 +1,6 @@
 use crate::k8s::K8sConfig;
 use anyhow::Result;
 use rustls::RootCertStore;
-use rustls_pki_types::CertificateDer;
 use rustls_native_certs::load_native_certs;
 use std::fs::File;
 use std::io::BufReader;
@@ -18,9 +17,9 @@ pub fn install(config: &K8sConfig) -> Result<RootCertStore> {
     let path = &config.cacrt_path;
     let file = File::open(path)?;
     let mut reader = BufReader::new(file);
-    let certs = rustls_pemfile::certs(&mut reader)?;
+    let certs = rustls_pemfile::certs(&mut reader);
     for cert in certs {
-        roots.add(CertificateDer::from(cert))?;
+        roots.add(cert?)?;
     }
 
     Ok(roots)
